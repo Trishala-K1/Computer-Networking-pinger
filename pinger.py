90924bbcf3c7a2152851d202ae101bf0fc2f41df
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-03-27 14:13:09 -0400

    Update pinger.py

--- pinger.py
+++ pinger.py
@@ -67,8 +67,8 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
             RTT = timeReceived - TimestampInPayload
             bytes = len(recPacket)
             ttl = str(recPacket[8:8])
-            remaining = (RTT*1000, bytes, ttl)
-            return(remaining)
+            remaining = ( bytes, ttl)
+            return(RTT*1000, remaining)
         else:
             return ['0', '0.0', '0', '0.0']
         # Fill in end
